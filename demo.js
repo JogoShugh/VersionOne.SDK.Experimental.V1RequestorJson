b8467b8781a69f4135ecdef00bebb5820a28cfd6
jogoshugh <jgough@JGough.corp.versionone.net>
2012-12-13 16:36:40 -0500

    Coffee 2 JS

--- demo.js
+++ demo.js
@@ -1,69 +1,89 @@
-projectName = "'System (All Projects)'"
-projectScopeId = null
+var headers, host, projectName, projectScopeId, queryOpts, service, setupApp, whereCriteria;
 
-host = "http://platform-dev"
+projectName = "'System (All Projects)'";
 
-service = host + "/CustomerTest/rest-1.v1/Data/"
+projectScopeId = null;
 
-queryOpts = acceptFormat: "hal+json"
+host = "http://platform-dev";
 
-headers = Authorization: "Basic " + btoa("admin:Admin101#")
+service = host + "/CustomerTest/rest-1.v1/Data/";
 
-whereCriteria = Name : projectName
-  
-$.ajax(
-    url: service + "Scope" + "?where=" + $.param(whereCriteria) + "&acceptFormat=hal%2bjson&sel="
-    headers: headers
-    type: "GET"
-  ).done (data) ->
-    projectScopeId = data._links.self.id    
-    setupApp()
+queryOpts = {
+  acceptFormat: "hal+json"
+};
 
-setupApp = ->    
-    fields = [
-      name: "RequestedBy"
-      label: "Requested By"
+headers = {
+  Authorization: "Basic " + btoa("admin:Admin101#")
+};
+
+whereCriteria = {
+  Name: projectName
+};
+
+$.ajax({
+  url: service + "Scope" + "?where=" + $.param(whereCriteria) + "&acceptFormat=hal%2bjson&sel=",
+  headers: headers,
+  type: "GET"
+}).done(function(data) {
+  projectScopeId = data._links.self.id;
+  return setupApp();
+});
+
+setupApp = function() {
+  var createDto, createStory, fields, init;
+  fields = [
+    {
+      name: "RequestedBy",
+      label: "Requested By",
       def: ""
-    ,  
-      name: "Name"
-      label: "Title"
+    }, {
+      name: "Name",
+      label: "Title",
       def: ""
-    ,
-      name: "Description"
-      label: "Description"
-      def: ""
-      type: "textarea" # TODO
+    }, {
+      name: "Description",
+      label: "Description",
+      def: "",
+      type: "textarea",
       height: 100
-    ]
-    
-    createStory = ->
-      dto = createDto()
-      
-      $.ajax(
-        url: service + "Request" + "?" + $.param(queryOpts)
-        headers: headers
-        type: "POST"
-        data: JSON.stringify(dto)
-        contentType: "hal+json"
-      ).done (data) ->
-        item = $("<div></div>")
-        item.html $("#requestItemTemplate").render(data)
-
-        $("#output").prepend item
-    
-    createDto = ->
-      attributes = {}
-      
-      $("#requestForm input[type='text']").each ->
-        el = $(this)
-        attributes[el.attr("id")] = el.val()
-        
-      attributes._links = Scope: { idref: projectScopeId }
-      
-      return attributes
-    
-    init = ->
-      $("#requestForm").html $("#fieldsTemplate").render {fields: fields}
-    
-    $ init
-    $("#run").click createStory​
+    }
+  ];
+  createStory = function() {
+    var dto;
+    dto = createDto();
+    return $.ajax({
+      url: service + "Request" + "?" + $.param(queryOpts),
+      headers: headers,
+      type: "POST",
+      data: JSON.stringify(dto),
+      contentType: "hal+json"
+    }).done(function(data) {
+      var item;
+      item = $("<div></div>");
+      item.html($("#requestItemTemplate").render(data));
+      return $("#output").prepend(item);
+    });
+  };
+  createDto = function() {
+    var attributes;
+    attributes = {};
+    $("#requestForm input[type='text']").each(function() {
+      var el;
+      el = $(this);
+      return attributes[el.attr("id")] = el.val();
+    });
+    attributes._links = {
+      Scope: {
+        idref: projectScopeId
+      }
+    };
+    return attributes;
+  };
+  init = function() {
+    return $("#requestForm").html($("#fieldsTemplate").render({
+      fields: fields
+    }));
+  };
+  $(init);
+  return $("#run").click(createStory​);
+};
